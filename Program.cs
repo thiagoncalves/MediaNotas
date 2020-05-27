d83742902a263ba6b3f1ac9774bf1bcb2ecce564
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-27 18:11:43 -0300

    First Version

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] notas = new double[3];
+
+            Console.WriteLine("Digite a primeira nota: ");
+            notas[0] = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Digite a segunda nota: ");
+            notas[1] = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Digite a terceira nota: ");
+            notas[2] = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            double media = notas.Average();
+
+            SaberResultado(media);
+
+            Console.ReadLine();
+        }
+
+        public static void SaberResultado (double media)
+        {
+            if (media == 10)
+                Console.WriteLine("Aprovado com Distinção");
+            if (media >= 7 && media < 10)
+                Console.WriteLine("Aprovado");
+            if (media < 7)
+                Console.WriteLine("Reprovado");
+        }
+    }
+}
